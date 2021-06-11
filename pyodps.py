89e8df0b3522044e3478486255328656c64b383a
wbnupku <wbnupku@gmail.com>
2021-06-11 09:40:58 +0800

    Update pyodps.py

--- pyodps.py
+++ pyodps.py
@@ -18,4 +18,24 @@ def milisecond2ds(t, format='%Y%m%d'):
     dt = datetime.fromtimestamp(int(t) / 1000)
     return dt.strftime(format)
 
-      
+
+def show_pts(tbl_name):
+    """Print table partition names."""
+    for pt in o.get_table(tbl_name).iterate_partitions():
+        print(pt.name)
+
+        
+def get_max_ds_pt(tbl_name, ignore_substr='.done'):
+    tbl = o.get_table(tbl_name)
+    def get_ds(pt_name):
+        items = pt_name.split(',')
+        for t in items:
+            if 'ds=' in t and ignore_substr not in t:
+                return t.split('=')[1].strip('\'')
+        return None
+    dslist = [get_ds(pt.name) for pt in tbl.iterate_partitions()]
+
+    dslist = [ds for ds in dslist if ds is not None and len(ds) == 8]
+    print(dslist)
+    max_ds = max(dslist)
+    return max_ds
